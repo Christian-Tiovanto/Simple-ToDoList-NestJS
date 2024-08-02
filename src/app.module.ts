72f00274e2d2c6ec5127ede70ed53c139b3d9f40
christian <christian@christian-pc.lan>
2024-08-02 13:11:36 +0700

    feat: adding dependency for TypeOrm Postgre, and adding docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './modules/user/models/user.model';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: 'SIMPLELEARNINGAPP',
+      database: process.env.DB_NAME,
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
